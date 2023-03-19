6f33c52695f61d25b51d260537301b5d5b85b92f
Limaaron <limaaron@vk.com>
2023-03-20 01:02:03 +0300

    Update

--- build.sh
+++ build.sh
@@ -9,26 +9,29 @@ _BASEPATH=`dirname "$0"`
 _BASEPATH=`sh -c "cd \"$_BASEPATH\" && pwd"`
 
 _KERNSRCPATH="/usr/src"
-_ROOTFS="/$_BASEPATH/$_OSNAME"
+_ROOTFS="/$_BASEPATH/$_OSNAME/build/rootfs"
 
-_KERNCONF="/$_ARCH/conf/$_OSNAME"
+_KERNSRCCONF="/$_BASEPATH/$_ARCH/conf/$_OSNAME"
+_KERNDESTCONF="/$_KERNSRCPATH/sys/$_ARCH/conf/$_OSNAME"
 _SRCCONF="/$_BASEPATH/src.conf"
 
 set -e
 cd $_KERNSRCPATH
-ln -s $_KERNCONF $_KERNSRCPATH$_KERNCONF
+ln -si $_KERNDESTCONF $_KERNSRCCONF
 
 echo "Making dirs..."
 mkdir -p $_ROOTFS
-make distrib-dirs -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNCONF SRCCONF=$_SRCCONF
+make distrib-dirs -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNDESTCONF SRCCONF=$_SRCCONF
 echo "Making dirs finished"
 
 echo "Building system..."
-make buildworld   -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNCONF SRCCONF=$_SRCCONF
-make buildkernel  -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNCONF SRCCONF=$_SRCCONF
+make buildworld   -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNDESTCONF SRCCONF=$_SRCCONF
+make buildkernel  -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNDESTCONF SRCCONF=$_SRCCONF
 echo "Building system finished"
 
 echo "Installing system..."
-meke installworld   -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNCONF SRCCONF=$_SRCCONF
-make installkernel  -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNCONF SRCCONF=$_SRCCONF
+make installworld   -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNDESTCONF SRCCONF=$_SRCCONF
+make installkernel  -j$_JOBS DESTDIR=$_ROOTFS KERNCONF=$_KERNDESTCONF SRCCONF=$_SRCCONF
 echo "Installing system finished"
+
+rm -f $_KERNDESTCONF
